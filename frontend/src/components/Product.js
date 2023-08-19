1e98a57218fabb7ff8d5bccd2fde39e28ea1d56d
Mohammed Ahbar Ajaib <ahbarajaib@Ahbars-MacBook-Pro.local>
2023-08-19 16:38:44 +0530

    from local

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -67,15 +67,20 @@ const Product = ({ product }) => {
       </Link>
       <Card.Body className="text-center d-flex flex-column">
         <Link to={`/product/${product?._id}`}>
-          <Card.Title as="div" style={{ minHeight: "40px" }}>
-            <strong>{product?.name}</strong>
+          <Card.Title as="div" style={{ minHeight: "60px", fontWeight: "600" }}>
+            {product?.name}
           </Card.Title>
         </Link>
-        <Card.Subtitle as="div" className="text-muted">
+        <Card.Subtitle
+          as="div"
+          className="text-muted"
+          style={{ fontSize: "0.75rem" }}
+        >
           {product?.brand}
         </Card.Subtitle>
+
         <div className="d-flex flex-column">
-          <div className="w-100 mb-3">
+          <div className="w-100 mb-2">
             <Dropdown size="sm">
               <Dropdown.Toggle
                 style={{
@@ -96,7 +101,12 @@ const Product = ({ product }) => {
                       key={price.qty}
                       active={selectedQty === price.qty}
                       onClick={() =>
-                        handleQtySelect(price.qty, price.units, price.price)
+                        handleQtySelect(
+                          price.qty,
+                          price.units,
+                          price.price,
+                          price.discount
+                        )
                       }
                     >
                       {price.qty} {price.units}
@@ -107,34 +117,31 @@ const Product = ({ product }) => {
           </div>
           <div className="w-100 text-right">
             <div className="price-container d-flex justify-content-center">
-              <Card.Text as="h4" className="mr-2" style={{ marginBottom: "0" }}>
+              <Card.Text as="h6" className="mr-2" style={{ marginBottom: "0" }}>
                 AED{" "}
-                {selectedQty
+                {selectedQty && product?.prices?.length > 0
                   ? (
                       selectedPrice *
-                      (1 - product.prices[0].discount / 100) *
+                      (1 -
+                        (product.prices.find(
+                          (price) => price.qty === selectedQty
+                        )?.discount || 0) /
+                          100) *
                       noOfProducts
                     ).toFixed(2)
-                  : (product?.prices && product.prices.length > 0
-                      ? product.prices[0].discountedPrice !== undefined
-                        ? product.prices[0].discountedPrice
-                        : product.prices[0].price
-                      : 0
-                    ).toFixed(2)}
+                  : "Calculation Error"}
               </Card.Text>
+
               {product?.prices[0]?.discount > 0 ? (
                 <Card.Text
-                  as="h6"
+                  as="p"
                   className="original-price"
-                  style={{ marginBottom: "0" }}
+                  style={{ marginBottom: "0", fontSize: "0.8em" }}
                 >
                   &nbsp;{" "}
-                  {selectedQty
+                  {selectedQty && product?.prices?.length > 0
                     ? (selectedPrice * noOfProducts).toFixed(2)
-                    : (product?.prices && product.prices.length > 0
-                        ? product.prices[0].price
-                        : 0
-                      ).toFixed(2)}
+                    : "Calculation Error"}
                 </Card.Text>
               ) : null}
             </div>
@@ -142,7 +149,7 @@ const Product = ({ product }) => {
         </div>
         <Button
           onClick={addToCartHandler}
-          className="button-primary mt-auto small-button"
+          className="button-primary mt-1 small-button"
           variant="primary"
         >
           Add to Cart
