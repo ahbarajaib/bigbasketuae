dfdb6e6eae2f653e85af78bbbca49af1c21fd182
Mohammed Ahbar Ajaib <ahbarajaib@gmail.com>
2024-06-15 16:47:12 +0400

    from local

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
-//es6 way of importing
 import path from "path";
-import { resolve } from "path";
 import stripe from "stripe";
 import https from "https";
 import fs from "fs";
@@ -9,7 +7,6 @@ import express from "express";
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import morgan from "morgan";
-//import products from './data/products.js'
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -24,9 +21,8 @@ import promotionRoutes from "./routes/promotionRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-import nStatic from "node-static";
 
-// importing environmental variables
+// Importing environmental variables
 dotenv.config();
 
 connectDB();
@@ -36,6 +32,7 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
 const corsOptions = {
   origin: process.env.CLIENT_URL,
   optionsSuccessStatus: 200,
@@ -44,50 +41,30 @@ app.use(cors(corsOptions));
 
 app.use(
   bodyParser.json({
-    verify: function (req, res, buf) {
+    verify: (req, res, buf) => {
       req.rawBody = buf;
     },
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
-app.use((req, res, next) => {
-  //to check which URL triggered this console.log(req.originalUrl)
-
-  next();
-});
-
-app.use(
-  bodyParser.json({
-    verify: function (req, res, buf) {
-      req.rawBody = buf;
-    },
-  })
-);
-
 const staticDir = process.env.STATIC_DIR;
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
-const stripeInstance = stripe(stripeSecretKey, {
-  apiVersion: "2022-08-01",
-});
+const stripeInstance = stripe(stripeSecretKey, { apiVersion: "2022-08-01" });
 
 app.use(express.static(staticDir));
 
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.originalUrl}`);
+  next();
+});
+
 app.get("/", (req, res) => {
-  // Your homepage logic
   res.send("Welcome to the homepage!");
 });
 
 app.use("/payment", paymentRoutes);
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -103,43 +80,18 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use("/banners", express.static(path.join(__dirname, "/banners")));
 
-var fileServer = new nStatic.Server("./public");
-
-// Define routes
-app.get("/api/contactus", (req, res) => {
-  // Handle GET request for /api/contactus
-  res.send("Contact us page");
-});
-
-app.get("/api/delivery", (req, res) => {
-  // Handle GET request for /api/delivery
-  res.send("Delivery FAQ page");
-});
-
-app.get("/api/returns", (req, res) => {
-  // Handle GET request for /api/returns
-  res.send("Returns page");
-});
-
-app.get("/api/aboutus", (req, res) => {
-  // Handle GET request for /api/aboutus
-  res.send("About us page");
-});
-
-app.get("/api/privacy", (req, res) => {
-  // Handle GET request for /api/privacy
-  res.send("Privacy policy page");
-});
-
-app.get("/api/terms", (req, res) => {
-  // Handle GET request for /api/terms
-  res.send("Terms page");
-});
+// Additional routes
+app.get("/api/contactus", (req, res) => res.send("Contact us page"));
+app.get("/api/delivery", (req, res) => res.send("Delivery FAQ page"));
+app.get("/api/returns", (req, res) => res.send("Returns page"));
+app.get("/api/aboutus", (req, res) => res.send("About us page"));
+app.get("/api/privacy", (req, res) => res.send("Privacy policy page"));
+app.get("/api/terms", (req, res) => res.send("Terms page"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) =>
-    res.sendFile(__dirname, "frontend", "build", "index.html")
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
@@ -147,16 +99,12 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-//@errorMiddleware.js middleware to handle which is not a path if entered route is not a
-//route in app then this will kick
 app.use(notFound);
-
-//error middleware @errorMiddleware.js
 app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 if (process.env.USE_SSL === "true") {
-  // Setup for HTTPS server
   const privateKey = fs.readFileSync(
     "/etc/letsencrypt/live/bigbasketuae.com/privkey.pem",
     "utf8"
@@ -177,7 +125,6 @@ if (process.env.USE_SSL === "true") {
     console.log(`HTTPS Server running on port ${PORT}`);
   });
 } else {
-  // Setup for HTTP server
   app.listen(PORT, () => {
     console.log(`HTTP Server running on port ${PORT}`);
   });
